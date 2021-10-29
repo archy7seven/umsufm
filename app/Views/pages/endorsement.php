994274fbc95bd01f6937e3176f704d66cab47abd
fikriansari <fikriansari.mfa@gmail.com>
2021-10-29 22:44:46 +0700

    mengubah template endorsement.php

--- app/Views/pages/endorsement.php
+++ app/Views/pages/endorsement.php
@@ -26,86 +26,72 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <div class="panel-title-box">
-                                <h3>Projects</h3>
-                                <span>Activity of projects by members</span>
+                                <h3>Endorsement</h3>
+                                <span>Berikut data Endorsement UMSU FM</span>
                             </div>
                             <ul class="panel-controls" style="margin-top: 2px;">
-                                <li><a href="index.html#" class="panel-refresh" data-toggle="tooltip" data-placement="top" title="Refresh"><span class="fa fa-refresh"></span></a></li>
-                                <li class="dropdown">
-                                    <a href="index.html#" class="dropdown-toggle" data-toggle="dropdown"><span class="fa fa-cog"></span></a>                                        
-                                    <ul class="dropdown-menu">
-                                        <li><a href="index.html#" class="panel-collapse"><span class="fa fa-angle-down"></span> Collapse</a></li>
-                                        <li><a href="index.html#" class="panel-remove"><span class="fa fa-times"></span> Remove</a></li>
-                                    </ul>                                        
-                                </li>                                        
+                                <button Type="button" class="btn btn-success" data-toggle="modal" data-target="#tambahEndorsement">Tambah Data</button>
+                                <button Type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteSelectedEndorsement">Hapus Data</button>                                        
                             </ul>
                         </div>
                         <div class="panel-body panel-body-table">
-                            
                             <div class="table-responsive">
                                 <table class="table table-bordered table-striped">
                                     <thead>
                                         <tr>
-                                            <th width="50%">Project</th>
-                                            <th width="20%">Status</th>
-                                            <th width="30%">Activity</th>
+                                            <th width=5% style="text-align:center">
+                                                <span class="custom-checkbox">
+                                                    <input type="checkbox" id="selectAll">
+                                                    <label for="selectAll"></label>
+                                                </span>
+                                            </th>
+                                            <th width="5%" style="text-align:center">No</th>
+                                            <th width="20%">Nama Endorsement</th>
+                                            <th width="10%">Tanggal Awal</th>
+                                            <th width="10%">Tanggal Akhir</th>
+                                            <th width="35%">Deskripsi</th>  
+                                            <th width="15%" style="text-align:center">Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td><strong>Atlant</strong></td>
-                                            <td><span class="label label-danger">Developing</span></td>
-                                            <td>
-                                                <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 85%;">85%</div>
-                                                </div>
+                                            <td style="text-align:center">
+                                                <span class="custom-checkbox">
+                                                    <input type="checkbox" id="checkbox1" name="options[]" value="1">
+                                                    <label for="checkbox1"></label>
+                                                </span>
+                                            </td>
+                                            <td style="text-align:center">1</td>
+                                            <td>Sample Endorsement</td>
+                                            <td>07-07-2021</td>
+                                            <td>07-12-2021</td>
+                                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur debitis nisi repudiandae sint voluptate, perferendis delectus aliquam ea, distinctio laborum repellat nobis quaerat eos dolores odit tenetur ratione, ipsa dolor!</td>
+                                            <td style="text-align:center">
+                                                <button Type="button" class="btn btn-primary" data-toggle="modal" data-target="#editEndorsement"><span class="fa fa-edit"></span></button>
+                                                <button Type="button" class="btn btn-danger" data-toggle="modal" data-target="#hapusEndorsement"><span class="fa fa-trash-o"></span></button>
+                                                <button Type="button" class="btn btn-default"><span class="fa fa-eye"></span></button>
                                             </td>
                                         </tr>
-                                        <tr>
-                                            <td><strong>Gemini</strong></td>
-                                            <td><span class="label label-warning">Updating</span></td>
-                                            <td>
-                                                <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">40%</div>
+                                        <!-- START MODAL DELETE ENDORSEMENT -->
+                                        <div class="modal fade" id="hapusEndorsement" arialabelledby="staticBackdropLabel" data-backdrop="static" data-keyboard="false" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <button type=button class="close" data-dismiss="modal" aria-label="Close"><div aria-hidden="true">&times;</div></button>
+                                                        <h3 class="modal-title" id="modalLabel"><strong>Hapus</strong> Data Endorsement</h3>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <p>Apakah kamu yakin ingin menghapus data Endorsement?</p>
+                                                        <p class="text-warning"><small>This action cannot be undone</small></p>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="close" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                        <button type="simpan" class="btn btn-danger">Delete</button>
+                                                    </div>
                                                 </div>
-                                            </td>
-                                        </tr>                                                
-                                        <tr>
-                                            <td><strong>Taurus</strong></td>
-                                            <td><span class="label label-warning">Updating</span></td>
-                                            <td>
-                                                <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 72%;">72%</div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td><strong>Leo</strong></td>
-                                            <td><span class="label label-success">Support</span></td>
-                                            <td>
-                                                <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td><strong>Virgo</strong></td>
-                                            <td><span class="label label-success">Support</span></td>
-                                            <td>
-                                                <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
-                                                </div>
-                                            </td>
-                                        </tr>                                                
-                                        <tr>
-                                            <td><strong>Aquarius</strong></td>
-                                            <td><span class="label label-success">Support</span></td>
-                                            <td>
-                                                <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
-                                                </div>
-                                            </td>
-                                        </tr>
+                                            </div>
+                                        </div>
+                                        <!-- END MODAL DELETE ENDORSEMENT -->
                                     </tbody>
                                 </table>
                             </div>
@@ -123,6 +109,141 @@
     <!-- END PAGE CONTENT -->
 </div>
 <!-- END PAGE CONTAINER -->
+
+<!-- START MODAL DELETE SELECTED ENDORSEMENT -->
+<div class="modal fade" id="deleteSelectedEndorsement" arialabelledby="staticBackdropLabel" data-backdrop="static" data-keyboard="false" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type=button class="close" data-dismiss="modal" aria-label="Close"><div aria-hidden="true">&times;</div></button>
+                <h3 class="modal-title" id="modalLabel"><strong>Hapus</strong> Data Endorsement</h3>
+            </div>
+            <div class="modal-body">
+                <p>Apakah kamu yakin ingin menghapus data Endorsement?</p>
+                <p class="text-warning"><small>This action cannot be undone</small></p>
+            </div>
+            <div class="modal-footer">
+                <button type="close" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="simpan" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END MODAL DELETE SELECTED ENDORSEMENT -->
+
+<!-- START MODAL TAMBAH ENDORSEMENT -->
+<div class="modal fade" id="tambahEndorsement" arialabelledby="staticBackdropLabel" data-backdrop="static" data-keyboard="false" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type=button class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title" id="modalLabel"><strong>Tambah</strong> Data Endorsement</h3>
+            </div>
+            <div class="modal-body">
+                <form role="form" class="form-horizontal">
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Flayer endorsement</label>
+                        <div class="col-md-9">
+                            <input type="file" accept="image/*" class="fileinput btn-default" name="filename3" id="filename3" data-filename-placement="inside" title="Browse"/>    
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Nama Endorsement</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" placeholder="Fill this field please" require/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Tanggal Awal</label>
+                        <div class="col-md-9">
+                            <div class="input-group date" id="dp-2" data-date="17-08-2021" data-date-format="dd-mm-yyyy">
+                                <input type="text" class="form-control" value="17-08-2021"/>
+                                <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Tanggal Akhir</label>
+                        <div class="col-md-9">
+                            <div class="input-group date" id="dp-2" data-date="17-08-2021" data-date-format="dd-mm-yyyy">
+                                <input type="text" class="form-control" value="17-08-2021"/>
+                                <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Deskripsi</label>
+                        <div class="col-md-9">
+                            <textarea class="form-control" require></textarea>
+                        </div>
+                    </div>  
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="close" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="simpan" class="btn btn-success">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END MODAL TAMBAH DATA ENDORSEMENT -->
+
+<!-- START MODAL EDIT ENDORSEMENT -->
+<div class="modal fade" id="editEndorsement" arialabelledby="staticBackdropLabel" data-backdrop="static" data-keyboard="false" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type=button class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title" id="modalLabel"><strong>Edit</strong> Data Endorsement</h3>
+            </div>
+            <div class="modal-body">
+                <form role="form" class="form-horizontal">
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Flayer endorsement</label>
+                        <div class="col-md-9">
+                            <input type="file" accept="image/*" class="fileinput btn-default" name="filename3" id="filename3" data-filename-placement="inside" title="Browse"/>    
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Nama Endorsement</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" placeholder="Fill this field please" require/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Tanggal Awal</label>
+                        <div class="col-md-9">
+                            <div class="input-group date" id="dp-2" data-date="17-08-2021" data-date-format="dd-mm-yyyy">
+                                <input type="text" class="form-control" value="17-08-2021"/>
+                                <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Tanggal Akhir</label>
+                        <div class="col-md-9">
+                            <div class="input-group date" id="dp-2" data-date="17-08-2021" data-date-format="dd-mm-yyyy">
+                                <input type="text" class="form-control" value="17-08-2021"/>
+                                <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Deskripsi</label>
+                        <div class="col-md-9">
+                            <textarea class="form-control" require></textarea>
+                        </div>
+                    </div>  
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="close" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="simpan" class="btn btn-success">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END MODAL EDIT DATA ENDORSEMENT -->
 
 <!-- MESSAGE BOX-->
 <div class="message-box animated fadeIn" data-sound="alert" id="mb-signout">
