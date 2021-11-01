99c33c72999a55054bab1cd3d11d5db6576d747f
fikriansari <fikriansari.mfa@gmail.com>
2021-11-01 15:43:18 +0700

    fix bugs

--- app/Views/layout/templateHome.php
+++ app/Views/layout/templateHome.php
@@ -1,66 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-    <head>        
-        <!-- META SECTION -->
-        <title><?= $title . " | ".$appName; ?></title>            
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <!-- END META SECTION -->
-        
-        <!-- CSS INCLUDE -->        
-        <link rel="stylesheet" type="text/css" id="theme" href="theme/css/theme-default.css"/>             
-        <!-- EOF CSS INCLUDE -->
-    </head>
-    <body>
-        
-        <?= $this->renderSection('content');?>
-
-        <!-- START PRELOADS -->
-        <audio id="audio-alert" src="audio/alert.mp3" preload="auto"></audio>
-        <audio id="audio-fail" src="audio/fail.mp3" preload="auto"></audio>
-        <!-- END PRELOADS -->                  
-        
-        <!-- START SCRIPTS -->
-        <!-- START PLUGINS -->
-        <script type="text/javascript" src="theme/js/plugins/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="theme/js/plugins/jquery/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="theme/js/plugins/bootstrap/bootstrap.min.js"></script>        
-        <script type='text/javascript' src='theme/js/plugins/popper/popper.min.js'></script>             
-        <!-- END PLUGINS -->
-
-        <!-- START THIS PAGE PLUGINS-->        
-        <script type='text/javascript' src='theme/js/plugins/icheck/icheck.min.js'></script>        
-        <script type="text/javascript" src="theme/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js"></script>
-        <script type="text/javascript" src="theme/js/plugins/scrolltotop/scrolltopcontrol.js"></script>
-        
-        <script type="text/javascript" src="theme/js/plugins/morris/raphael-min.js"></script>
-        <script type="text/javascript" src="theme/js/plugins/morris/morris.min.js"></script>       
-        <script type="text/javascript" src="theme/js/plugins/rickshaw/d3.v3.js"></script>
-        <script type="text/javascript" src="theme/js/plugins/rickshaw/rickshaw.min.js"></script>
-        <script type='text/javascript' src='theme/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js'></script>
-        <script type='text/javascript' src='theme/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js'></script>                
-        <script type='text/javascript' src='theme/js/plugins/bootstrap/bootstrap-datepicker.js'></script>  
-        <script type="text/javascript" src="theme/js/plugins/owl/owl.carousel.min.js"></script>
-        <script type="text/javascript" src="theme/js/plugins/bootstrap/bootstrap-file-input.js"></script>     
-        <script type="text/javascript" src="theme/js/plugins/bootstrap/bootstrap-select.js"></script> 
-        <script type="text/javascript" src="theme/js/plugins/bootstrap/bootstrap-timepicker.min.js"></script>
-        <script type="text/javascript" src="theme/js/plugins/bootstrap/bootstrap-datepicker.js"></script>                   
-        <!-- END THIS PAGE PLUGINS-->        
-
-        <!-- START TEMPLATE -->
-        <script type="text/javascript" src="theme/js/settings.js"></script>
-        <script type="text/javascript" src="theme/js/plugins.js"></script>        
-        <script type="text/javascript" src="theme/js/actions.js"></script>
-        <script type="text/javascript" src="theme/js/demo_dashboard.js"></script>
-        <!-- END TEMPLATE -->
-        <!-- END TIME -->
-    <!-- END SCRIPTS -->         
-    </body>
-=======
 
 <head>
     <!-- META SECTION -->
@@ -127,5 +66,4 @@
 
 </body>
 
->>>>>>> 0fc686a01cafe4ac4c82c04b87c319412a5065d5
 </html>
