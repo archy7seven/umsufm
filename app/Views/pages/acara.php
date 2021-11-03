c45c6e5993a5d54040df07be2d0dd99617c27d39
fikriansari <fikriansari.mfa@gmail.com>
2021-11-03 11:48:27 +0700

    fix bugs

--- app/Views/pages/acara.php
+++ app/Views/pages/acara.php
@@ -87,7 +87,7 @@
                                                                         <div class="form-group">
                                                                             <label class="col-md-3 control-label">Flayer Acara</label>
                                                                             <div class="col-md-9">
-                                                                                <input type="file" accept="image/*" class="fileinput btn-danger" name="flayer" id="filename3" data-filename-placement="inside" title="Browse..." />
+                                                                                <input type="file" accept="image/png" class="fileinput btn-danger" name="flayer" id="filename3" data-filename-placement="inside" title="Browse..." />
                                                                             </div>
                                                                         </div>
                                                                         <div class="form-group">
@@ -166,11 +166,13 @@
                                                                         </div>
                                                                         <div class="form-group">
                                                                             <label class="col-md-3 control-label">Arsip Acara</label>
-                                                                            <label class="switch col-md-9">
-                                                                                <input type="checkbox" checked value="0" />
-                                                                                <span></span>
-                                                                            </label>
-                                                                        </div>
+                                                                            <div class="col-md-9">
+                                                                                <label class="switch">
+                                                                                    <input type="checkbox" checked value="0" />
+                                                                                    <span></span>
+                                                                                </label>
+                                                                            </div>
+                                                                            </div>
                                                                     </form>
                                                                 </div>
                                                                 <div class="modal-footer">
@@ -326,10 +328,12 @@
                     </div>
                     <div class="form-group">
                         <label class="col-md-3 control-label">Arsip Acara</label>
-                        <label class="switch col-md-9">
-                            <input type="checkbox" checked name="acaraArsip" />
-                            <span></span>
-                        </label>
+                        <div class="col-md-9">
+                            <label class="switch">
+                                <input type="checkbox" checked name="acaraArsip" />
+                                <span></span>
+                            </label>
+                        </div>
                     </div>
 
                 </div>
