e4d4b163efeb5bb6cc1664a522b19bffdaa8952a
fikriansari <fikriansari.mfa@gmail.com>
2021-11-07 12:45:07 +0700

    layouting register page

--- vendor/myth/auth/src/Views/layout.php
+++ vendor/myth/auth/src/Views/layout.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="favicon.ico" type="image/x-icon" />
-    <title>UMSU FM</title>
+    <title><?= lang('Auth.appName') ?></title>
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" type="text/css" id="theme" href="<?= base_url('theme/css/theme-default.css'); ?>" />
 </head>
