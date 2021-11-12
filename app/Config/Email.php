0abb4ad3e8960361100f1ce95ccc0dd07f7e5fff
fikriansari <fikriansari.mfa@gmail.com>
2021-11-12 10:00:09 +0700

    ubah nama email

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -14,7 +14,7 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromName = 'BSI UMSU';
+    public $fromName = 'Biro Sistem Informasi UMSU';
 
     /**
      * @var string
